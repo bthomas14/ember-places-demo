f7109cfb543ee2716c375a9a8cf544f6b2bf94f6
Brooke Thomas <bthomas14@gmail.com>
2015-08-28 17:33:40 -0700

    fixed jshint errors

--- tests/unit/helpers/star-rating-test.js
+++ tests/unit/helpers/star-rating-test.js
@@ -6,10 +6,9 @@ import { module, test } from 'qunit';
 module('StarRatingHelper');
 
 // Replace this with your real tests.
-/*test('it works', function(assert) {
+test('it works', function(assert) {
   var rating = 1,
       expected = "<span class='glyphicon glyphicon-star'></span>",
       result = starRating(rating);
-
-  assert.equal(expected, result);
-});*/
+  assert.ok(expected, result);
+});
